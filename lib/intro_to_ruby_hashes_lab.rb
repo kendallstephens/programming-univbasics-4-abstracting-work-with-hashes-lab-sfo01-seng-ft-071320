e242df061a157ffa9f47f365ce00f001d3fd4fd7
Kendall Stephens <kendoll.stephens@gmail.com>
2020-07-01 22:37:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -9,10 +9,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key]
-  hash[key] ?
-  hash[key] += 1 :
-  hash[key] = 1
+  hash[key]
+hash[key] ?
+hash[key] += 1 :
+hash[key] = 1
   hash
 end
 
